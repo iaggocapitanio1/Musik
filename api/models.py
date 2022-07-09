bdfabd30489ebeab49d2cef28304a9a288adf088
iaggocapitanio1 <iaggo.capitanio@gmail.com>
2022-07-09 18:32:15 +0100

    adding requirements

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+
+def generate_unique_code():
+    import string
+    import random
+    length = 6
+    while True:
+        code = "".join(random.choice(string.ascii_uppercase, k=length ))
+        if Room.objects.filter(code=code).count()==0:
+            break
+    return code 
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.ImageField(null=False, default=1)
+    created_at = models.DateField(auto_now_add=True)
+
+    
